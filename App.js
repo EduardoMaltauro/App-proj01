d81c62e0298b7ee92c146cee7a45d6d8f09119d5
EduardoMaltauro <maltauroeduardo717@gmail.com>
2024-03-16 11:31:31 -0300

    React Native

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+
+const estilo = StyleSheet.create({
+  tela: {
+    backgroundColor: "#19282F"
+  },
+
+  titulo: {
+    color: "#FF6464",
+    fontSize: 48,
+    marginVertical: 32,
+    textAlign: "center"
+  },
+
+  texto: {
+    color: "#aaa",
+    fontSize: 20,
+    margin: 32
+  },
+
+  imagem: {
+    height: 200,
+    width: "100%" 
+  }
+})
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  return <ScrollView style={estilo.tela}>
+    <StatusBar barStyle="light-content" backgroundColor="#19282F" />
+    <View>
+      <Text style={estilo.titulo}> Aplicativo TOP </Text>
+      <Text style={estilo.texto}>Meu primeiro APP com React Native</Text>
+      <Image style={estilo.imagem} source={{ uri: "https://picsum.photos/200/200" }}/>
     </View>
-  );
+  </ScrollView>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
